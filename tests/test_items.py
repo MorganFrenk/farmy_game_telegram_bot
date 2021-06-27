6ffd5438b8c820a981505a5cfa3de25606e08433
MorganFrenk <morganfrenk2@gmail.com>
2021-06-27 23:44:33 +0300

    Expand Items test

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,18 +1,27 @@
 import unittest
-from objects import items
+from objects.items import Items, Item
 
 
 class TestItems(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.item_1 = items.Item('item one', 1, 100)
-        self.item_2 = items.Item('item two', 2, 200)
-        self.items_1 = items.Items()
-
-    def test_items(self):
-        self.items_1.add_item(self.item_1)
-        self.assertTrue(self.item_1 in self.items_1.items)
+        self.item_1 = Item('item one', 1, 100)
+        self.item_2 = Item('item two', 2, 200)
+        self.items_pool = Items()
+        self.items_pool.add_item(self.item_1)
 
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_add_items(self) -> None:
+        self.assertTrue(self.item_1 in self.items_pool.items)
+
+    def test_remove_items(self) -> None:
+        self.items_pool.remove_item(self.item_1)
+        self.assertFalse(self.item_1 in self.items_pool.items)
+
+    def test_find_items(self) -> None:
+        self.assertEqual(self.item_1, self.items_pool.find_item(self.item_1.name))
+
+    def test_list_items(self) -> None:
+        self.items_pool.add_item(self.item_2)
+        self.assertEqual([self.item_1], self.items_pool.list_items_with_power(self.item_1.power))
+        self.assertEqual([self.item_1, self.item_2], self.items_pool.list_items_with_power(self.item_2.power))
