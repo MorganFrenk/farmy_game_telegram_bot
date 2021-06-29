aa55c06aeacd9862c5f3e266038b4157c4ce4ee5
MorganFrenk <morganfrenk2@gmail.com>
2021-06-29 20:54:22 +0300

    Few fixes in items unittests

--- tests/test_items.py
+++ tests/test_items.py
@@ -11,16 +11,16 @@ class TestItems(unittest.TestCase):
 
     def test_add_items(self) -> None:
         self.items_pool.add_item(self.item_1)
-        self.assertTrue(self.item_1 in self.items_pool.items)
+        self.assertIn(self.item_1, self.items_pool.items)
 
     def test_remove_items(self) -> None:
         self.items_pool.add_item(self.item_1)
         self.items_pool.remove_item(self.item_1)
-        self.assertFalse(self.item_1 in self.items_pool.items)
+        self.assertNotIn(self.item_1, self.items_pool.items)
 
     def test_add_multiple_items(self) -> None:
         self.items_pool.add_item([self.item_1, self.item_2])
-        self.assertTrue(self.item_1 and self.item_2 in self.items_pool.items)
+        self.assertIn(self.item_1 and self.item_2, self.items_pool.items)
 
     def test_find_items(self) -> None:
         self.items_pool.add_item(self.item_1)
@@ -29,53 +29,54 @@ class TestItems(unittest.TestCase):
     def test_list_items(self) -> None:
         self.items_pool.add_item(self.item_1)
         self.items_pool.add_item(self.item_2)
-        self.assertEqual([self.item_1], self.items_pool.list_items_with_power(self.item_1.power))
+        self.assertIn(self.item_1, self.items_pool.list_items_with_power(self.item_1.power))
 
-        self.assertEqual([self.item_1, self.item_2], self.items_pool.list_items_with_power(self.item_2.power))
+        self.assertIn(self.item_1 and self.item_2, self.items_pool.list_items_with_power(self.item_2.power))
 
-        self.assertEqual([self.item_1, self.item_2],
-                         self.items_pool.list_items_with_power(10, power_limit=self.item_1.power))
-        self.assertEqual([self.item_2],
-                         self.items_pool.list_items_with_power(10, power_limit=self.item_2.power))
+        self.assertIn(self.item_1 and self.item_2,
+                      self.items_pool.list_items_with_power(10, power_limit=self.item_1.power))
+        self.assertIn(self.item_2,
+                      self.items_pool.list_items_with_power(10, power_limit=self.item_2.power))
 
     def test_random_items(self) -> None:
         self.items_pool.add_item(self.item_1)
-        self.assertTrue(self.item_1 in self.items_pool.get_random_items())
+        self.assertIn(self.item_1, self.items_pool.get_random_items())
 
         self.items_pool.add_item(self.item_2)
-        self.assertTrue(self.item_1 and self.item_2 in self.items_pool.get_random_items(amount=2))
+        self.assertIn(self.item_1 and self.item_2, self.items_pool.get_random_items(amount=2))
 
-        self.assertTrue(self.item_1 in self.items_pool.get_random_items(power=1))
+        self.assertIn(self.item_1, self.items_pool.get_random_items(power=1))
 
-        self.assertTrue(self.item_1 and self.item_2 in self.items_pool.get_random_items(power=2, amount=2))
+        self.assertIn(self.item_1 and self.item_2, self.items_pool.get_random_items(power=2, amount=2))
 
     def test_shop_refill(self) -> None:
         shop = Shop()
-        self.item_1 = Item('item', 10, 200)
-        self.item_2 = Item('item', 10, 200)
-        self.item_3 = Item('item', 10, 200)
-        self.item_4 = Item('item', 10, 200)
-        self.item_5 = Item('item', 10, 200)
+        self.item_1 = Item('item', 4, 200)
+        self.item_2 = Item('item', 5, 200)
+        self.item_3 = Item('item', 6, 200)
+        self.item_4 = Item('item', 7, 200)
+        self.item_5 = Item('item', 8, 200)
 
-        self.item_6 = Item('item', 20, 200)
-        self.item_7 = Item('item', 20, 200)
-        self.item_8 = Item('item', 20, 200)
-        self.item_9 = Item('item', 20, 200)
+        self.item_6 = Item('item', 16, 200)
+        self.item_7 = Item('item', 17, 200)
+        self.item_8 = Item('item', 18, 200)
+        self.item_9 = Item('item', 19, 200)
 
-        self.item_10 = Item('item', 30, 300)
-        self.item_11 = Item('item', 30, 300)
-        self.item_12 = Item('item', 30, 300)
+        self.item_10 = Item('item', 27, 300)
+        self.item_11 = Item('item', 28, 300)
+        self.item_12 = Item('item', 29, 300)
 
-        self.item_13 = Item('item', 40, 300)
-        self.item_14 = Item('item', 40, 300)
+        self.item_13 = Item('item', 38, 300)
+        self.item_14 = Item('item', 39, 300)
 
-        self.item_15 = Item('item', 50, 300)
+        self.item_15 = Item('item', 49, 300)
 
         self.items_pool.add_item([self.item_1, self.item_2, self.item_3, self.item_4, self.item_5,
                                  self.item_6, self.item_7, self.item_8, self.item_9, self.item_10,
                                  self.item_11, self.item_12, self.item_13, self.item_14, self.item_15])
 
         shop.refill_shop(items_pool=self.items_pool)
-        self.assertTrue(self.item_1 and self.item_2 and self.item_3 and self.item_4 and self.item_5 and
-                        self.item_6 and self.item_7 and self.item_8 and self.item_9 and self.item_10 and
-                        self.item_11 and self.item_12 and self.item_13 and self.item_14 and self.item_15 in shop.items)
+
+        self.assertCountEqual(shop.items, [self.item_1, self.item_2, self.item_3, self.item_4, self.item_5,
+                                           self.item_6, self.item_7, self.item_8, self.item_9, self.item_10,
+                                           self.item_11, self.item_12, self.item_13, self.item_14, self.item_15])
