00e02e480070a8490002fce807c87246840b2cbc
MorganFrenk <morganfrenk2@gmail.com>
2021-06-30 22:28:34 +0300

    Start test locations

--- tests/test_locations.py
+++ tests/test_locations.py
@@ -1,3 +1,18 @@
 import unittest
 from objects.locations import Location
+from objects.items import Items, Item
 from objects.heroes import Hero
+
+
+class TestItems(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.items_pool = Items()
+        self.item_1 = Item('item', 19, 200)
+        self.item_2 = Item('item', 39, 200)
+        self.item_3 = Item('item', 79, 200)
+        self.item_4 = Item('item', 159, 200)
+        self.item_5 = Item('item', 319, 200)
+        self.items_pool.add_item([self.item_1, self.item_2, self.item_3, self.item_4, self.item_5])
+        self.test_location = Location('test loc', 19, self.items_pool, pool_amount=1)
+        
